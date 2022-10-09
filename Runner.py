334dd83a93d83d1b0b8e83a496ce6836546d6a4c
shaynebersin <95396826+shaynebersin@users.noreply.github.com>
2022-10-09 09:40:58 -0400

    Update Runner.py

--- Runner.py
+++ Runner.py
@@ -2,7 +2,7 @@ import rank_calculator
 import csv
 
 schoolslink = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4d8JvuxteLJ7NqAvZhjYzRggjV_ptKUQCNNsQAVrblK9r2h3CFovSODtSpg7Jp7_xt0lFdLjxUedQ/pub?output=csv"
-coedRegattaLink = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRYuBfRn534EQy6tXw957Ree0IPLUxyaFri25OQtd_0n5SyG3J-5ELfpUPgwUKGxT_qfDmzrmtds8Y2/pub?output=csv"
+coedRegattaLink = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSwVtHxq6LONV5TiX73WF1xaCDk93xnsFz7Vh6VisLJFJIg6MpvsVNDCoOe-XcBc6h1cu1SC6_HYPVM/pub?output=csv"
 womensRegattaLink = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRrN9Sdev0TpLnAzuUgPQFwEa_VXcmsXX9CKoR3Y5p4fFydyh8WwdM1Xx-yaOWobLlNYPYWUMjGql1w/pub?output=csv"
 
 coed_rankings_output_file = "rankings.csv"
